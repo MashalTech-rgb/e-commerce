8fa021530bef4a88316c5dc1ddd90e2caac562fd
MashalTech-rgb <mashalpugc@gmail.com>
2025-03-11 17:41:36 +0500

    comit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
         {/* Logo Section */}
         <div className="flex items-center space-x-2">
           <img
-            src="/src/assets/logo-colored.png" // Change this to your logo path
+            src="/images/logo-colored.png" // Change this to your logo path
             alt="Brand Logo"
             className="w-28 md:w-32 h-auto" // Adjust logo size
           />

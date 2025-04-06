5a42541f339a801345591803fde33559cc86b208
MashalTech-rgb <mashalpugc@gmail.com>
2025-04-06 22:17:46 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-// App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
-import Layout from './pages/Layout';
-import Listing from './pages/listing'; 
-import ProductDetail from './pages/ProductDetail';
-const App = () => {
-  return (
-    <> 
-      <Routes>
-        <Route path="/" element={<Layout />} /> 
-        <Route path="/listing" element={<Listing />} /> 
-        <Route path="/products/:id" element={<ProductDetail />} />
-      </Routes>
-    </>
-  );
-};
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import ProductDetail from "./pages/ProductDetail";
+import Listing from "./pages/listing";
 
-export default App;
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/listing" element={<Listing/>} />
+        <Route path="/products/:id" element={<ProductDetail />} /> {/* Add this route */}
+      </Routes>
+    </Router>
+  );
+}

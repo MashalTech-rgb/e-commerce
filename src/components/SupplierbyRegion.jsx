6e442ee5717f3ab1530705e16b36ce48090e3a4b
MashalTech-rgb <mashalpugc@gmail.com>
2025-04-11 19:43:49 +0500

    commit

--- src/components/SupplierbyRegion.jsx
+++ src/components/SupplierbyRegion.jsx
@@ -6,61 +6,61 @@ const SuppliersByRegion = () => {
       id: 1,
       name: 'Arabic Emirates',
       shop: 'shopname.ae',
-      flag: '/public/assets/images/dubai.png', // URL for UAE flag
+      flag: '/assets/images/dubai.png', // URL for UAE flag
     },
     {
       id: 2,
       name: 'Denmark',
       shop: 'dennmark.com.dk',
-      flag: '/public/assets/images/scotland.png', // URL for Denmark flag
+      flag: '/assets/images/scotland.png', // URL for Denmark flag
     },
     {
       id: 3,
       name: 'Australia',
       shop: 'shopname.ae',
-      flag: '/public/assets/images/australia.png', // URL for Australia flag
+      flag: '/assets/images/australia.png', // URL for Australia flag
     },
     {
       id: 4,
       name: 'France',
       shop: 'shopname.com.fr',
-      flag: '/public/assets/images/france.png', // URL for France flag
+      flag: '/assets/images/france.png', // URL for France flag
     },
     {
       id: 5,
       name: 'United States',
       shop: 'shopname.ae',
-      flag: '/public/assets/images/usa.png', // URL for USA flag
+      flag: '/assets/images/usa.png', // URL for USA flag
     },
     {
       id: 6,
       name: 'Arabic Emirates',
       shop: 'shopname.ae',
-      flag: '/public/assets/images/dubai.png', // URL for UAE flag
+      flag: '/assets/images/dubai.png', // URL for UAE flag
     },
     {
       id: 7,
       name: 'Russia',
       shop: 'shopname.ru',
-      flag: '/public/assets/images/russia.png', // URL for Russia flag
+      flag: '/assets/images/russia.png', // URL for Russia flag
     },
     {
       id: 8,
       name: 'China',
       shop: 'shopname.ae',
-      flag: '/public/assets/images/china.png', // URL for China flag
+      flag: '/assets/images/china.png', // URL for China flag
     },
     {
       id: 9,
       name: 'Italy',
       shop: 'shopname.it',
-      flag: '/public/assets/images/itlay.png', // URL for Italy flag
+      flag: '/assets/images/itlay.png', // URL for Italy flag
     },
     {
       id: 10,
       name: 'Great Britain',
       shop: 'shopname.co.uk',
-      flag: '/public/assets/images/england.png', // URL for UK flag
+      flag: '/assets/images/england.png', // URL for UK flag
     },
   ];
 
